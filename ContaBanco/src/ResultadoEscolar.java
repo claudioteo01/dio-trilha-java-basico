3a38a3e5e10f7f210d0f8f687fa9be2a09417e02
claudioteo01 <klaudioteo@gmail.com>
2024-06-11 12:17:06 -0300

    Condicao ternaria

--- ContaBanco/src/ResultadoEscolar.java
+++ ContaBanco/src/ResultadoEscolar.java
@@ -2,7 +2,14 @@ public class ResultadoEscolar {
 
     public static void main(String[] args) {
 
-        int nota = 1;
+        int nota = 5;
+        // Condição Ternaria
+
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+
+
+        /*
         
         if(nota >= 7)
          System.out.println("Aprovado");
@@ -13,7 +20,7 @@ public class ResultadoEscolar {
 
          else
         
-         System.out.println("Reprovado");
+         System.out.println("Reprovado");*/
      }
     
 }
